ef16d0371d7829ce46dc8eb677f2aab3244f2631
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-15 11:52:36 +0100

    config : add  docker-compose and  app configuration

---
+++ src/main/java/ma/youcode/Al/Baraka/Digital/AlBarakaDigitalApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.Al.Baraka.Digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlBarakaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlBarakaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/Al/Baraka/Digital/AlBarakaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.Al.Baraka.Digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlBarakaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
