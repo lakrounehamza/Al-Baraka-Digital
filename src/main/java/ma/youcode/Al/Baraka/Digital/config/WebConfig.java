bd2faa9c21c3f733a3e9020a669164898023e4a4
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-18 01:19:06 +0100

    feat :  add configuration jwt  a webConfig

--- src/main/java/ma/youcode/Al/Baraka/Digital/config/WebConfig.java
+++ src/main/java/ma/youcode/Al/Baraka/Digital/config/WebConfig.java
@@ -1,18 +1,66 @@
 package ma.youcode.Al.Baraka.Digital.config;
 
+import lombok.AllArgsConstructor;
+import ma.youcode.Al.Baraka.Digital.enums.UserRole;
+import ma.youcode.Al.Baraka.Digital.security.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthTokenFilter  authTokenFilter(){
+        return   new AuthTokenFilter();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        if (userDetailsService instanceof UserDetailsPasswordService) {
+            provider.setUserDetailsPasswordService((UserDetailsPasswordService) userDetailsService);
+        }
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity  httpSecurity){
         httpSecurity.csrf(csrf ->  csrf.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->
-                        auth.requestMatchers("/api/auth/**").permitAll());
+                        auth.requestMatchers("/api/auth/signin","/api/auth/signup").permitAll()
+                                .requestMatchers("/api/auth/logout").hasAuthority("CLIENT")
+                                .anyRequest().authenticated())
+                .authenticationProvider(daoAuthenticationProvider());
+        httpSecurity.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
