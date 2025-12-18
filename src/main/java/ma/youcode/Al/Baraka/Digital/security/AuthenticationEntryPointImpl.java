895d27f1d4cb9148af996c9c906c4d2743b54f87
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-18 01:16:44 +0100

    feat: add implementation AuthenticationEntryPoint to manage error autherization  403

---
+++ src/main/java/ma/youcode/Al/Baraka/Digital/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,18 @@
+package ma.youcode.Al.Baraka.Digital.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"error "+authException.getMessage());
+    }
+}
