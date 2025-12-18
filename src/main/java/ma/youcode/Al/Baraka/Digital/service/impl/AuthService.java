c0e0d73c66d0584fdfeea800f85163bc2644df4b
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-18 01:17:53 +0100

    feat(service) :  add authentification service

--- src/main/java/ma/youcode/Al/Baraka/Digital/service/impl/AuthService.java
+++ src/main/java/ma/youcode/Al/Baraka/Digital/service/impl/AuthService.java
@@ -3,16 +3,22 @@ package ma.youcode.Al.Baraka.Digital.service.impl;
 import lombok.AllArgsConstructor;
 import ma.youcode.Al.Baraka.Digital.dto.request.LoginRequestDto;
 import ma.youcode.Al.Baraka.Digital.dto.request.UserRequestDto;
+import ma.youcode.Al.Baraka.Digital.dto.response.LoginResponseDto;
 import ma.youcode.Al.Baraka.Digital.dto.response.UserResponseDto;
 import ma.youcode.Al.Baraka.Digital.entity.User;
 import ma.youcode.Al.Baraka.Digital.exception.DuplicateUserException;
+import ma.youcode.Al.Baraka.Digital.exception.NotFoundException;
 import ma.youcode.Al.Baraka.Digital.mapper.UserMapper;
 import ma.youcode.Al.Baraka.Digital.repository.UserRepository;
+import ma.youcode.Al.Baraka.Digital.security.JwtUtil;
 import ma.youcode.Al.Baraka.Digital.service.interfaces.IAuthService;
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +26,7 @@ public class AuthService implements IAuthService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private  final JwtUtil jwtUtil;
 
     @Override
     public UserResponseDto signup(UserRequestDto requset) {
@@ -33,12 +40,31 @@ public class AuthService implements IAuthService {
     }
 
     @Override
-    public UserResponseDto signin(LoginRequestDto request) {
-        return null;
+    public LoginResponseDto signin(LoginRequestDto request) {
+        Optional<User> user = userRepository.findByUsername(request.username());
+        if(!user.isPresent())
+            throw new NotFoundException(" username  incoricte");
+        User user1  = user.get();
+        if(!BCrypt.checkpw(request.password(),user1.getPassword()))
+            throw new NotFoundException(" username  incoricte");
+        UserResponseDto  userResponse   = userMapper.toDto(user1);
+        String  token  = jwtUtil.generateToken(user1);
+        return new LoginResponseDto(userResponse,token);
     }
 
     @Override
     public Map<String, String> logout() {
-        return null;
+        SecurityContextHolder.clearContext();
+        return Map.of("se","ok");
+    }
+    @Override
+    public UserResponseDto profil(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username  = authentication.getName();
+        Optional<User>   user  = userRepository.findByUsername(username);
+        if (!user.isPresent())
+            return null;
+        return userMapper.toDto(user.get());
     }
 }
--- src/main/java/ma/youcode/Al/Baraka/Digital/service/interfaces/IAuthService.java
+++ src/main/java/ma/youcode/Al/Baraka/Digital/service/interfaces/IAuthService.java
@@ -2,12 +2,14 @@ package ma.youcode.Al.Baraka.Digital.service.interfaces;
 
 import ma.youcode.Al.Baraka.Digital.dto.request.LoginRequestDto;
 import ma.youcode.Al.Baraka.Digital.dto.request.UserRequestDto;
+import ma.youcode.Al.Baraka.Digital.dto.response.LoginResponseDto;
 import ma.youcode.Al.Baraka.Digital.dto.response.UserResponseDto;
 
 import java.util.Map;
 
 public interface IAuthService {
     UserResponseDto signup(UserRequestDto request);
-    UserResponseDto signin(LoginRequestDto request);
+    LoginResponseDto signin(LoginRequestDto request);
     Map<String,String> logout();
+    UserResponseDto profil();
 }
