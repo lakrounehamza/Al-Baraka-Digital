988baa63b71aaf2348dc5e0232736e454a9299f8
lakrounehamza <lakrounehamza10@gmail.com>
2025-12-20 23:57:26 +0100

    feat (entity- dto) : ajouter  entity Account avec  dto

---
+++ src/main/java/ma/youcode/Al/Baraka/Digital/dto/response/AccountResponse.java
@@ -0,0 +1,16 @@
+package ma.youcode.Al.Baraka.Digital.dto.response;
+
+import ma.youcode.Al.Baraka.Digital.entity.Operation;
+import ma.youcode.Al.Baraka.Digital.entity.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record AccountResponse(
+        User onwer,
+        String numer,
+        BigDecimal balance,
+        List<Operation> operationsSource,
+        List<Operation> operationsDestination
+) {
+}
--- src/main/java/ma/youcode/Al/Baraka/Digital/entity/Account.java
+++ src/main/java/ma/youcode/Al/Baraka/Digital/entity/Account.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import ma.youcode.Al.Baraka.Digital.service.impl.AccountNumerManagment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,8 +25,13 @@ public class Account {
     private User onwer;
     private String numer;
     private BigDecimal balance;
-    @OneToMany(mappedBy = "accountSource_id")
-    List<Operation> operationsSource;
-    @OneToMany(mappedBy = "accountDestination_id")
-    List<Operation> operationsDestination;
+    @OneToMany(mappedBy = "accountSource")
+    List<Operation> operationsSource =new ArrayList<>();
+    @OneToMany(mappedBy = "accountDestination")
+    List<Operation> operationsDestination = new ArrayList<>();
+
+    @PrePersist
+    private  void  init(){
+        balance = BigDecimal.ZERO;
+    }
 }
